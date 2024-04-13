b1973d5fcbde2c40e6a364a8f58ab5782cd261fa
Juan Esteban Paez Alfonso <jesteban_paez@javeriana.edu.co>
2024-04-13 04:27:33 +0000

    COMPLETADO

--- demo/src/main/java/com/example/demo/controllers/HolaMundoController.java
+++ demo/src/main/java/com/example/demo/controllers/HolaMundoController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,7 +10,22 @@ public class HolaMundoController {
 
     @GetMapping("/")
     public String hola() {
-        return "Hola Mundo";
-    }
+       return "Hola Mundo";
+     }
+
+   @GetMapping("/saludo")
+  public String saludo(@RequestParam String nombre) {
+      return "Hola " + nombre;
+  }
+
+  @GetMapping("/suma")
+  public String Suma(@RequestParam Integer a, @RequestParam Integer b) {
+      return "" + (a + b);
+  }
+
+  @GetMapping("/validar/{clase}")
+  public String validar(@PathVariable String clase, @RequestParam String id) {
+      return "validando la clase " + clase +  " y el id " + id;
+  }
 
 }
